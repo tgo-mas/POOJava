645c3fb997b8d89e4bde61c583ea4683bc1bf9b3
Thomas Almeida <99335340+tgo-mas@users.noreply.github.com>
2022-11-22 09:05:52 -0300

    Add classes Dev e User

---
+++ src/model/Dev.java
@@ -0,0 +1,86 @@
+package model;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Dev extends User {
+	
+	private Set<Bootcamp> btcInscritos;
+	private Set<Bootcamp> btcConcluidos;
+	private Set<Conteudo> contInscritos;
+	private Set<Conteudo> contConluidos;
+	
+	public Dev(String username, String email, String passwd) {
+		super(username, email, passwd);
+		this.btcInscritos = new LinkedHashSet<>();
+		this.btcConcluidos = new LinkedHashSet<>();
+		this.contInscritos = new LinkedHashSet<>();
+		this.contConluidos = new LinkedHashSet<>();
+	}
+
+	//// addBootcamp(btc) -> Inscreve o Dev no Bootcamp passado por parâmetro.
+	
+	public void addBootcamp(Bootcamp btc) {
+		
+		if(!this.btcInscritos.add(btc)) {
+			throw new Error("O Dev já está inscrito neste Bootcamp.");
+		}
+		
+	}
+	
+	//// concluirCont(cont) -> Conclui um conteúdo no qual o dev estava inscrito.
+	
+	public void concluirCont(Conteudo cont) {
+		
+		if(this.contInscritos.contains(cont)) {
+			this.contInscritos.remove(cont);
+			this.contConluidos.add(cont);
+		}else {
+			throw new Error("O Dev não está inscrito neste bootcamp.");
+		}
+		
+	}
+	
+	//// makeXpTotal() -> Calcula e retorna o xp total do Dev (Seus conteúdos concluídos)
+	
+	public double makeXpTotal() {
+		double soma = 0;
+		for(Conteudo cont:contConluidos) {
+			soma += cont.makeXp();
+		}
+		return soma;
+	}
+	
+	public Set<Bootcamp> getBtcInscritos() {
+		return btcInscritos;
+	}
+
+	public void setBtcInscritos(Set<Bootcamp> btcInscritos) {
+		this.btcInscritos = btcInscritos;
+	}
+
+	public Set<Bootcamp> getBtcConcluidos() {
+		return btcConcluidos;
+	}
+
+	public void setBtcConcluidos(Set<Bootcamp> btcConcluidos) {
+		this.btcConcluidos = btcConcluidos;
+	}
+
+	public Set<Conteudo> getContInscritos() {
+		return contInscritos;
+	}
+
+	public void setContInscritos(Set<Conteudo> contInscritos) {
+		this.contInscritos = contInscritos;
+	}
+
+	public Set<Conteudo> getContConluidos() {
+		return contConluidos;
+	}
+
+	public void setContConluidos(Set<Conteudo> contConluidos) {
+		this.contConluidos = contConluidos;
+	}
+	
+}
